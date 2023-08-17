1c1eab7bd0a829680fc493792454c8b934a8ced1
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-17 13:19:51 +0300

    fix linter

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RoomsController < ApplicationController
-    def index
-        render json: {message: "rooms here"}
-    end
+  def index
+    render json: { message: 'rooms here' }
+  end
 end
