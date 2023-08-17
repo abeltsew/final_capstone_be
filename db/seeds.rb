4469577c06dc3e9683236bba9add920be85da975
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-17 22:07:06 +0300

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create cities
+cities = []
+20.times do |i|
+  cities << City.create(name: "City #{i + 1}")
+end
+
+# Create users
+users = []
+20.times do |i|
+  users << User.create(username: "User #{i + 1}")
+end
+
+# Create rooms
+rooms = []
+20.times do |i|
+  rooms << Room.create(
+    name: "Room #{i + 1}",
+    user: users.sample,
+    price: rand(50..300),
+    description: "Description for Room #{i + 1}"
+  )
+end
+
+# Create reservations
+20.times do |i|
+  Reservation.create(
+    user: users.sample,
+    room: rooms.sample,
+    city: cities.sample,
+    date: Date.today + rand(1..30).days
+  )
+end
