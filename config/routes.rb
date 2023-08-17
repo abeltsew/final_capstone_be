2946ed45a3455c49aaff13cbb5f4c475ecc95b41
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-17 13:18:21 +0300

    Added API/V1 route

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,19 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  namespace :API do
-    namespace :V1 do
+  namespace :api do
+    namespace :v1 do
       resources :users
       resources :rooms
       resources :reservations
       resources :cities
     end
   end
+
+  # namespace :API do
+  #   namespace :V1 do
+  #     get '/login', to: 'users#login'
+  #   end
+  # end
 
 end
