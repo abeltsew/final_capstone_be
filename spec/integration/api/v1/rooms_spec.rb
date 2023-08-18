14b757c6d502fc8956fd0576b57331474b59f8a4
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-18 10:51:59 +0300

    Add Swaggger for Room API

---
+++ spec/integration/api/v1/rooms_spec.rb
@@ -0,0 +1,115 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/rooms', type: :request do
+  path '/api/v1/rooms' do
+    get('list rooms') do
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create room') do
+      consumes 'application/json'
+      produces 'application/json'
+
+      parameter name: :room, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          description: { type: :string },
+          price: { type: :number },
+          user_id: { type: :integer }
+        },
+        required: %w[name description price user_id]
+      }
+
+      response(200, 'successful') do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 description: { type: :string },
+                 price: { type: :number },
+                 user_id: { type: :integer },
+                 created_at: { type: :string, format: 'date-time' },
+                 updated_at: { type: :string, format: 'date-time' }
+               },
+               required: %w[id name description price user_id created_at updated_at]
+
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/rooms/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show room') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('update room') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('update room') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete room') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
