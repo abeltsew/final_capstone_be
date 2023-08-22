4c680ff3d981acba3da4dd1f1a52d649115bee54
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-22 14:01:06 +0300

    Added Swagger docs

---
+++ spec/integration/api/v1/reservations_spec.rb
@@ -0,0 +1,56 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/reservations', type: :request do
+  before(:each) do
+    @user = User.create(username: 'johnd')
+    @room = Room.create(name: 'King size', user: @user, price: 456)
+    @city = City.create(name: 'London')
+    @reservation = Reservation.create(room_id: @room.id, user_id: @user.id, date: Date.today, city_id: @city.id)
+  end
+
+  path '/api/v1/reservations' do
+    get('list reservations') do
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+  path '/api/v1/reservations' do
+    post('create reservation') do
+      consumes 'application/json'
+      produces 'application/json'
+      let(:reservation) { @reservation }
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          date: { type: :string },
+          city_id: { type: :integer },
+          room_id: { type: :integer },
+          user_id: { type: :integer }
+        },
+        required: %w[city_id room_id date user_id]
+      }
+      response(201, 'successful') do
+        schema type: :object,
+               properties: {
+                 date: { type: :date },
+                 user_id: { type: :integer },
+                 room_id: { type: :integer },
+                 city_id: { type: :integer },
+                 created_at: { type: :string, format: 'date-time' },
+                 updated_at: { type: :string, format: 'date-time' }
+               },
+               required: %w[city_id room_id date user_id]
+
+        run_test!
+      end
+    end
+  end
+end
