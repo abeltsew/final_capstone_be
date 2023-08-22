47ba68e6abb5105bc747ee94e1a1224ba686eef8
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-22 15:49:22 +0300

    Add docs for reservations

--- spec/integration/api/v1/reservations_spec.rb
+++ spec/integration/api/v1/reservations_spec.rb
@@ -10,18 +10,32 @@ RSpec.describe 'api/v1/reservations', type: :request do
 
   path '/api/v1/reservations' do
     get('list reservations') do
+      parameter name: 'user_id', in: :path, type: :integer, required: true
+      let(:user_id) { User.create(username: 'johnd') }
       response(200, 'successful') do
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+        schema type: :array,
+               items: {
+                 type: :object,
+                 properties: {
+                   date: { type: :date },
+                   user_id: { type: :integer },
+                   room_id: { type: :integer },
+                   city_id: { type: :integer },
+                   created_at: { type: :string, format: 'date-time' },
+                   updated_at: { type: :string, format: 'date-time' }
+                 },
+                 required: %w[city_id room_id date user_id]
+               }
+        it('should return success') do
+          user_id = @user.id
+          get '/api/v1/reservations', params: { user_id: }
+          expect(response).to have_http_status(:success)
         end
         run_test!
       end
     end
   end
+
   path '/api/v1/reservations' do
     post('create reservation') do
       consumes 'application/json'
